48ed018d5ffaaf88bc9dd19242ed28e19712077e
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-11-13 16:48:53 -0300

    adiciona e testa typography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '@alfabit-alura/button';
+import { TypographyComponent } from '@alfabit-alura/typography';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, TypographyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
