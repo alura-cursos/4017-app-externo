6e9913d3970924c4670ebf13735915944894429d
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-11-14 15:20:45 -0300

    adiciona e testa modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '@alfabit-alura/button';
 import { TypographyComponent } from '@alfabit-alura/typography';
+import { ModalComponent } from '@alfabit-alura/modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, TypographyComponent],
+  imports: [ButtonComponent, TypographyComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-externo';
+  modalOpen = false;
 }
